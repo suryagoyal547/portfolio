421ca0a06601cf0a256f92cf835ff3e9129e2b44
suryagoyal547 <97739266+suryagoyal547@users.noreply.github.com>
2023-09-06 18:06:17 +0530

    Update Projects.js
    Added a new project Random Quote Generator (React.Js) .

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -5,10 +5,18 @@ import timer from './assets/timer.png'
 import weather from './assets/weather.png'
 import imageGen from './assets/image-generator.png'
 import portfolio from './assets/portfolio.png'
+import random from './assets/random.png'
 
 const Projects = () => {
 
   const projects = [
+        {
+      imageSrc: portfolio,
+      title: "Portfolio Website",
+      overview: "A Portfolio Website having Skills,projects,contact and About section",
+      liveLink: "",
+      githubLink: "https://github.com/suryagoyal547/portfolio",
+    },
     {
       imageSrc: weather,
       title: "Weather-App (React.Js)",
@@ -16,27 +24,32 @@ const Projects = () => {
       liveLink: "https://64e50f8c8968a91750438a01--incredible-tartufo-7ecdd3.netlify.app/",
       githubLink: "https://github.com/suryagoyal547/react_weather_app",
     },
+
+    {
+      imageSrc: random,
+      title: "Random Quote Generator (React.Js)",
+      overview: "A quote Generator which fetches quotes using type-fit API",
+      liveLink: "https://main--mellow-cobbler-0308dd.netlify.app/",
+      githubLink: "https://github.com/suryagoyal547/Random-Quote-Generator",
+    },
+
+        {
+      imageSrc: imageGen,
+      title: "Image Search Engine (HTML, CSS, Javascript)",
+      overview: "An online image generator which takes user's input and Generates image using Unsplash API",
+      liveLink: "https://subtle-puffpuff-67a5af.netlify.app/",
+      githubLink: "https://github.com/suryagoyal547/Image-Generator",
+    },
+
     {
       imageSrc: timer,
       title: "Pomodoro-Timer(HTML, CSS, Javascript)",
       overview: "A pomodoro timer having timer set to 25 minutes and 3 buttons: start, stop, reset having corresponding functionalities",
       liveLink: "https://gleeful-ganache-a6bb3a.netlify.app/",
       githubLink: "https://github.com/suryagoyal547/timer_clock",
-    },
-    {
-      imageSrc: imageGen,
-      title: "Image Generator (HTML, CSS, Javascript)",
-      overview: "An online image generator which takes user's input and Generates image using Unsplash API",
-      liveLink: "https://subtle-puffpuff-67a5af.netlify.app/",
-      githubLink: "https://github.com/suryagoyal547/Image-Generator",
-    },
-    {
-      imageSrc: portfolio,
-      title: "Portfolio Website",
-      overview: "A Portfolio Website having Skills,projects,contact and About section",
-      liveLink: "",
-      githubLink: "https://github.com/suryagoyal547/portfolio",
     }
+
+
   ];
 
   return (
